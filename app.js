20f04de148b2a462c0e56ddece8ddd3160532bcf
naingwinphyoe <naingwinphyoe.19@gmail.com>
2024-02-20 16:03:09 +0630

    'Ep-16-Ejs-View-Engine'

--- app.js
+++ app.js
@@ -3,25 +3,25 @@ const express = require('express');
 
 const app = express();
 
+app.set('views', './views')
+app.set('view engine', 'ejs')
+
 app.get('/',(req,res) => {
-    res.sendFile('./views/home.html', {root : __dirname})
+    res.render('home');
 });
 
 app.get('/about',(req,res) => {
-    res.sendFile('./views/about.html', {root : __dirname})
+    res.render('about');
 });
 
-app.get('/about-us',(req,res) => {
-    res.redirect('/about');
-});
 
 app.get('/contact',(req,res) => {
-    res.sendFile('./views/contact.html', {root : __dirname})
+    res.render('contact');
 });
 
 app.use((req,res) => {
-    res.status(404);
-    res.sendFile('./views/404.html', {root : __dirname})
+    res.status(404).render('404');
+    
 });
 
 app.listen(3000,() => {
